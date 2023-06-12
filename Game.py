7bc316c39e2f6066919aeb4f2931510b90b1d32c
BasilMan69 <billkhoi2006@gmail.com>
2023-06-12 09:38:02 +0700

    add quit feature

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 from ConnectFour import ConnectFour
+from Caro import Caro
 
 def find_game_by_name(games, name):
     for game in games:
@@ -9,15 +10,24 @@ def choose_game(games):
     chosen_game_index = 0
     index_bol = False
     chosen_game_name = ''
+    quit_index = len(games)+1
     print("Games: ")
     for i in range(len(games)):
         print(f"\t{i+1}. {games[i].name}")
-    chosen_game_str = input("Choose one above: ")
+    print(f"\t{quit_index}. QUIT")
+    print(f"Choose one above (or enter {quit_index} to quit): ")
+    chosen_game_str = input()
     try:
         chosen_game_index = int(chosen_game_str) - 1
+        if chosen_game_index == quit_index - 1:
+            print("See you next time.")
+            return None
         index_bol = True
     except ValueError:
         chosen_game_name = chosen_game_str.lower()
+        if chosen_game_name == 'quit':
+            print("See you next time.")
+            return None
 
     if chosen_game_name not in [game.name.lower() for game in games] and chosen_game_index not in range(len(games)):
         print("Not an available game. Please choose again.")
@@ -31,6 +41,8 @@ def choose_game(games):
 def play_on_loop(games):
     print("What game do you want to play: ")
     game_to_play = choose_game(games)
+    if game_to_play == None:
+        return
     game_to_play.play()
     choice = ''
     while choice not in ["yes", "no"]:
@@ -53,5 +65,7 @@ def play_games(games):
 
 games = []
 cf = ConnectFour()
+xo = Caro()
 games.append(cf)
+games.append(xo)
 play_games(games)
